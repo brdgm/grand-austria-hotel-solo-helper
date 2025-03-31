d5b99f08619512eaf8a4ec6d53898e27c7f82777
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-03-31 20:57:22 +0200

    add icons

--- src/services/enum/GuestCategory.ts
+++ src/services/enum/GuestCategory.ts
@@ -2,10 +2,10 @@
  * Guest Category
  */
 enum GuestCategory {
-  BLUE_NOBLE = 'noble',
-  YELLOW_ARTIST = 'artist',
-  RED_CITIZEN = 'citizen',
-  GREEN_TOURIST = 'tourist',
+  BLUE_NOBLE = 'blue-noble',
+  YELLOW_ARTIST = 'yellow-artist',
+  RED_CITIZEN = 'red-citizen',
+  GREEN_TOURIST = 'green-tourist',
   ANY = 'any'
 }
 export default GuestCategory

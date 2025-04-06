883616b14afd0bc6f3b0eadef1cc6760a73cc931
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-04-06 12:20:11 +0200

    Scoring Screen (#7)

---
+++ src/components/scoring/FinalAmounts.vue
@@ -0,0 +1,177 @@
+<template>
+  <table>
+    <tbody>
+      <tr>
+        <th scope="col">
+        </th>
+        <th scope="col">
+          <span>{{t('endOfGame.player')}}</span>
+        </th>
+        <th scope="col">
+          <span>{{t(`opponentName.${deckType}`)}}</span>
+        </th>
+      </tr>
+      <tr>
+        <th scope="row">
+          <span v-html="t('endOfGameAmounts.scoringTrack')"></span>
+        </th>
+        <td v-for="index in playerCount" :key="index">
+          <input type="number" min="0" step="1" v-model="amount.scoringTrackVP[index-1]" @focus="inputSelectAll"/>
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">
+          <span v-html="t('endOfGameAmounts.staffCards')"></span>
+        </th>
+        <td v-for="index in playerCount" :key="index">
+          <input type="number" min="0" step="1" v-model="amount.staffCardsVP[index-1]" @focus="inputSelectAll"/>
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">
+          <span v-html="t('endOfGameAmounts.occupiedRoomsLevel4')"></span>
+        </th>
+        <td v-for="index in playerCount" :key="index">
+          <input type="number" min="0" step="1" v-model="amount.occupiedRoomsLevel4[index-1]" @focus="inputSelectAll"/>
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">
+          <span v-html="t('endOfGameAmounts.occupiedRoomsLevel3')"></span>
+        </th>
+        <td v-for="index in playerCount" :key="index">
+          <input type="number" min="0" step="1" v-model="amount.occupiedRoomsLevel3[index-1]" @focus="inputSelectAll"/>
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">
+          <span v-html="t('endOfGameAmounts.occupiedRoomsLevel2')"></span>
+        </th>
+        <td v-for="index in playerCount" :key="index">
+          <input type="number" min="0" step="1" v-model="amount.occupiedRoomsLevel2[index-1]" @focus="inputSelectAll"/>
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">
+          <span v-html="t('endOfGameAmounts.occupiedRoomsLevel1')"></span>
+        </th>
+        <td v-for="index in playerCount" :key="index">
+          <input type="number" min="0" step="1" v-model="amount.occupiedRoomsLevel1[index-1]" @focus="inputSelectAll"/>
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">
+          <span v-html="t('endOfGameAmounts.money')"></span>
+        </th>
+        <td>
+          <input type="number" min="0" step="1" v-model="amount.money[0]" @focus="inputSelectAll"/>
+        </td>
+        <td></td>
+      </tr>
+      <tr>
+        <th scope="row">
+          <span v-html="t('endOfGameAmounts.dishesDrinks')"></span>
+        </th>
+        <td>
+          <input type="number" min="0" step="1" v-model="amount.dishesDrinks[0]" @focus="inputSelectAll"/>
+        </td>
+        <td></td>
+      </tr>
+      <tr>
+        <th scope="row">
+          <span v-html="t('endOfGameAmounts.remainingGuests')"></span>
+        </th>
+        <td>
+          <input type="number" min="0" step="1" v-model="amount.remainingGuests[0]" @focus="inputSelectAll"/>
+        </td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
+
+  <button class="btn btn-primary btn-lg mt-4" @click="next()">
+    {{t('action.next')}}
+  </button>
+
+</template>
+
+<script lang="ts">
+import { useStateStore, FinalScoringAmount } from '@/store/state'
+import getMatchingDeckType from '@/util/getMatchingDeckType'
+import { defineComponent } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { useRouter } from 'vue-router'
+
+export default defineComponent({
+  name: 'FinalAmounts',
+  setup() {
+    const { t } = useI18n()
+    const state = useStateStore()
+    const router = useRouter()
+    const deckType = getMatchingDeckType(state.setup.expansions)
+
+    const amount = state.finalScoringAmount ?? 
+      {
+        scoringTrackVP: [],
+        staffCardsVP: [],
+        occupiedRoomsLevel4: [],
+        occupiedRoomsLevel3: [],
+        occupiedRoomsLevel2: [],
+        occupiedRoomsLevel1: [],
+        money: [],
+        dishesDrinks: [],
+        remainingGuests: []
+      } as FinalScoringAmount
+
+    const playerCount = 2
+
+    return { t, state, router, amount, playerCount, deckType }
+  },
+  methods: {
+    inputSelectAll(event: Event) : void {
+      const input = event.target as HTMLInputElement
+      input.select()
+    },
+    toNumber(value? : number) {
+      if (typeof value == 'string') {
+        return 0
+      }
+      else {
+        return value ?? 0
+      }
+    },
+    next() : void {
+      this.state.finalScoringAmount = this.amount
+      this.router.push(`/endOfGame`)
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  height: 2.5rem;
+  margin-right: 0.25rem;
+  &.small {
+    width: 2rem;
+    height: 2rem;
+  }
+}
+th, td {
+  text-align: center;
+  padding: 0.5rem;
+}
+tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+th {
+  white-space: nowrap;
+  vertical-align: middle;
+}
+th:nth-child(1) {
+  font-weight: normal;
+}
+input {
+  width: 5rem;
+}
+</style>
--- src/views/EndOfGame.vue
+++ src/views/EndOfGame.vue
@@ -1,20 +1,7 @@
 <template>
   <h1 class="mb-3">{{t('endOfGame.title')}}</h1>
 
-  <h5>{{t('endOfGame.finalScoring')}}</h5>
-  <ul>
-    <li v-html="t('endOfGame.staffCards')"></li>
-    <li v-html="t('endOfGame.occupiedRooms')"></li>
-    <li v-html="t('endOfGame.moneyDishDrinks')"></li>
-    <li v-html="t('endOfGame.remainingGuests')"></li>
-    <ul>
-      <li>
-        <span class="fst-italic" v-html="t('rules.botUniqueHotel.hautel-couture.title')"></span>:
-        <span v-html="t('rules.botUniqueHotel.hautel-couture.endOfGame')"></span>
-      </li>
-    </ul>
-  </ul>
-  <p class="fst-italic" v-html="t('endOfGame.winCondition')"></p>
+  <FinalScoring :amount="amount"/>
 
   <FooterButtons :backButtonRouteTo="backButtonRouteTo" endGameButtonType="endGame"/>
 </template>
@@ -23,39 +10,38 @@
 import { defineComponent } from 'vue'
 import { useI18n } from 'vue-i18n'
 import FooterButtons from '@/components/structure/FooterButtons.vue'
-import { useRoute } from 'vue-router'
-import { useStateStore } from '@/store/state'
-import NavigationState from '@/util/NavigationState'
-import BotUniqueHotel from '@/services/enum/BotUniqueHotel'
-import Expansion from '@/services/enum/Expansion'
+import { FinalScoringAmount, useStateStore } from '@/store/state'
+import FinalScoring from '@/components/scoring/FinalScoring.vue'
 
 export default defineComponent({
   name: 'EndOfGame',
   components: {
-    FooterButtons
+    FooterButtons,
+    FinalScoring
   },
   setup() {
     const { t } = useI18n()
-    const route = useRoute()
     const state = useStateStore()
 
-    const navigationState = new NavigationState(route, state)
+    const amount = state.finalScoringAmount ?? 
+      {
+        scoringTrackVP: [],
+        staffCardsVP: [],
+        occupiedRoomsLevel4: [],
+        occupiedRoomsLevel3: [],
+        occupiedRoomsLevel2: [],
+        occupiedRoomsLevel1: [],
+        money: [],
+        dishesDrinks: [],
+        remainingGuests: []
+      } as FinalScoringAmount
 
-    return { t, state, navigationState }
+    return { t, state, amount }
   },
   computed: {
     backButtonRouteTo() : string {
-      return '/round/7/end'
-    },
-    isBotUniqueHotelHautelCouture() : boolean {
-      return this.isBotUniqueHotel(BotUniqueHotel.HAUTEL_COUTURE)
-    },
-  },
-  methods: {
-    isBotUniqueHotel(botUniqueHotel : BotUniqueHotel) : boolean {
-      return this.state.setup.expansions.includes(Expansion.LETS_WALTZ_MODULE_3_UNIQUE_HOTELS)
-          &&  this.state.setup.botUniqueHotel == botUniqueHotel
-    },
+      return '/endOfGameAmounts'
+    }
   }
 })
 </script>
